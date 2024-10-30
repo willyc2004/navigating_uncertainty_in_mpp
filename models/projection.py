d3d708b3f3572a4a3f0b46fa4f5f03ddd267e900
jaiv <jaiv@itu.dk>
2024-10-30 10:21:32 +0100

    add linear violation sample option

--- models/projection.py
+++ models/projection.py
@@ -189,6 +189,7 @@ class ProjectionFactory:
         'convex_program': ConvexProgramLayer,
         'linear_program': LinearProgramLayer,
         'linear_violation':LinearViolationAdaption,
+        "linear_violation_sample": LinearViolationAdaption,
     }
 
     @staticmethod
