198d9b2a1d42218b27058fcaee84d7896ff4cc79
jaiv <jaiv@itu.dk>
2024-10-30 14:30:22 +0100

    update pi runs

---
+++ analyze_results.py
@@ -0,0 +1,19 @@
+import json
+import numpy as np
+
+# Load JSON file
+s = 16
+with open(f'results_scenario_tree_s{s}_pi.json', 'r') as file:
+    data = json.load(file)
+
+obj = []
+time = []
+for x in data:
+    obj.append(x["obj"])
+    time.append(x["time"])
+
+print(np.mean(obj))
+print(np.std(obj))
+
+print(np.mean(time))
+print(np.std(time))
