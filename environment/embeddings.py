970bd864b6840f1641e988ebf00aa10ca7b83643
jaiv <jaiv@itu.dk>
2024-11-07 16:17:38 +0100

    print shape

--- environment/embeddings.py
+++ environment/embeddings.py
@@ -166,6 +166,8 @@ class DynamicSinusoidalPositionalEncoding(nn.Module):
         pe = torch.zeros(seq_length, self.embed_dim, device=x.device)
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
+        print(pe.shape)
+
         pe = pe.unsqueeze(0).expand(batch_size, -1, -1)
         return x + pe
 
