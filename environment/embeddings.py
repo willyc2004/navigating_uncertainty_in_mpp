87d0af4c16e6f2c1be3bdaeb57a4225e55f5c040
jaiv <jaiv@itu.dk>
2024-11-07 16:09:49 +0100

    fix device

--- environment/embeddings.py
+++ environment/embeddings.py
@@ -162,7 +162,7 @@ class DynamicSinusoidalPositionalEncoding(nn.Module):
     def forward(self, x):
         seq_length = x.size(1)
         position = torch.arange(seq_length, dtype=torch.float, device=x.device).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, self.embed_dim, 2).float() * -(math.log(10000.0) / seq_length))
+        div_term = torch.exp(torch.arange(0, self.embed_dim, 2, device=x.device).float() * -(math.log(10000.0) / seq_length))
         pe = torch.zeros(seq_length, self.embed_dim, device=x.device)
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
