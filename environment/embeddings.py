1178768faec84f10a9da924ab10acec9f7f59a44
jaiv <jaiv@itu.dk>
2024-11-08 15:09:56 +0100

    allow for n_step pass

--- environment/embeddings.py
+++ environment/embeddings.py
@@ -11,8 +11,12 @@ from rl4co.utils.ops import gather_by_index
 class MPPInitEmbedding(nn.Module):
     def __init__(self, embed_dim, action_dim, env, num_constraints=5):
         super(MPPInitEmbedding, self).__init__()
+        # Store environment and sequence size
         self.env = env
+        self.seq_size = self.env.T * self.env.K
+
         # Initialize embeddings and layers
+        self.cache_initialized = False  # Flag to check if cache has been created
         # Category embeddings
         self.origin_port = nn.Embedding(self.env.P, embed_dim)
         self.destination_port = nn.Embedding(self.env.P, embed_dim)
@@ -27,30 +31,34 @@ class MPPInitEmbedding(nn.Module):
         self.stdx_demand = nn.Linear(1, embed_dim)
         self.fc = nn.Linear(8 * embed_dim, embed_dim)
         self.positional_encoding = DynamicSinusoidalPositionalEncoding(embed_dim*8) # 8 is the number of embeddings
-        self.cache_initialized = False  # Flag to check if cache has been created
 
-    def initialize_cache(self,):
-        # Compute and store fixed embeddings in cache
-        self.origin_emb_cache = self.origin_port(self.env.pol[:-1].to(torch.int64)).unsqueeze(0)
-        self.destination_emb_cache = self.destination_port(self.env.pod[:-1].to(torch.int64)).unsqueeze(0)
-        self.class_embed_cache = self.cargo_class(self.env.k[:-1].to(torch.int64)).unsqueeze(0)
-        self.weight_emb_cache = self.weight(self.env.weights[self.env.k[:-1]].view(-1, 1)).unsqueeze(0)
-        self.teu_embd_cache = self.teu(self.env.teus[self.env.k[:-1]].view(-1, 1)).unsqueeze(0)
+    def initialize_cache(self):
+        """Initialize cache for fixed embeddings"""
+        self.origin_emb_cache = self.origin_port(self.env.pol[:-1].to(torch.int64)).view(1,self.seq_size, -1)
+        self.destination_emb_cache = self.destination_port(self.env.pod[:-1].to(torch.int64)).view(1,self.seq_size, -1)
+        self.class_embed_cache = self.cargo_class(self.env.k[:-1].to(torch.int64)).view(1,self.seq_size, -1)
+        self.weight_emb_cache = self.weight(self.env.weights[self.env.k[:-1]].view(-1, 1)).view(1,self.seq_size, -1)
+        self.teu_embd_cache = self.teu(self.env.teus[self.env.k[:-1]].view(-1, 1)).view(1,self.seq_size, -1)
         self.revenue_emb_cache = self.revenue((self.env.revenues[:-1] /
-                                               (self.env.pod[:-1] - self.env.pol[:-1])).view(-1, 1)).unsqueeze(0)
+                                               (self.env.pod[:-1] - self.env.pol[:-1])).view(-1, 1)).view(1,self.seq_size, -1)
         self.cache_initialized = True  # Update flag
 
     def forward(self, td: TensorDict):
         # Get batch size and sequence length
-        batch_size, step_size = td["POL"].shape
+        if td["POL"].dim() == 2:
+            batch_size, seq_size = td["POL"].shape
+            expected_demand = self.ex_demand(td["expected_demand"].view(batch_size, seq_size, 1))
+            std_demand = self.stdx_demand(td["std_demand"].view(batch_size, seq_size, 1))
+        elif td["POL"].dim() == 3:
+            batch_size, n_step, seq_size = td["POL"].shape
+            expected_demand = self.ex_demand(td["expected_demand"][:,0].view(batch_size, seq_size, 1))
+            std_demand = self.stdx_demand(td["std_demand"][:,0].view(batch_size, seq_size, 1))
+        else:
+            raise ValueError("Invalid shape for POL")
 
         # Initialize cache if not done yet
         if not self.cache_initialized:
             self.initialize_cache()
-
-        # Compute only demand-related embeddings dynamically
-        expected_demand = self.ex_demand(td["expected_demand"].view(batch_size, step_size, 1))
-        std_demand = self.stdx_demand(td["std_demand"].view(batch_size, step_size, 1))
 
         # Concatenate all embeddings (using cached values for fixed ones)
         combined_emb = torch.cat([
@@ -59,7 +67,7 @@ class MPPInitEmbedding(nn.Module):
             self.destination_emb_cache.expand(batch_size, -1, -1),
             self.class_embed_cache.expand(batch_size, -1, -1),
             self.weight_emb_cache.expand(batch_size, -1, -1),
-            self.teu_embd_cache.expand(batch_size, -1, -1),
+            self.teu_embd_cache.expand(batch_size,-1, -1),
             self.revenue_emb_cache.expand(batch_size, -1, -1),
         ], dim=-1)
         # Final projection
@@ -88,7 +96,7 @@ class MPPContextEmbedding(nn.Module):
         self.violation = nn.Linear(5, embed_dim)
         self.rhs = nn.Linear(5, embed_dim)
         self.lhs_A = nn.Linear(action_dim * 5, embed_dim)
-        self.project_context = nn.Linear(embed_dim * 14, embed_dim, )
+        self.project_context = nn.Linear(embed_dim * 8, embed_dim, )
 
         # Self-attention layer
         # self.demand = SelfAttentionStateMapping(feature_dim=demand_dim, embed_dim=embed_dim, device=device)
@@ -100,49 +108,45 @@ class MPPContextEmbedding(nn.Module):
 
         # Get init embedding and state embedding
         select_init_embedding = gather_by_index(init_embeddings, td["episodic_step"])
-        state_embedding = self._state_embedding(init_embeddings, td)
+        state_embedding = self._state_embedding(td)
 
         # Project state, concat embeddings, and project concat to output
         context_embedding = torch.cat([select_init_embedding, state_embedding], dim=-1)
         output = self.project_context(context_embedding)
         return output
 
-    def _state_embedding(self, embeddings, td):
+    def _state_embedding(self, td):
         """Embed the state for the MPP.
         Important:
         - The state embedding size should not depend on e.g. voyage length and cargo types.
         - todo: It does depend on vessel size now, but this could be changed.
         """
-        # Demand
-        current_demand = self.current_demand(td["obs"]["current_demand"].view(td.batch_size[0], -1))
-        expected_demand = self.expected_demand(
-            torch.sum(td["obs"]["expected_demand"].view(td.batch_size[0], -1), dim=-1, keepdim=True))
-        std_demand = self.std_demand(
-            torch.sum(td["obs"]["std_demand"].view(td.batch_size[0], -1), dim=-1, keepdim=True))
-        observed_demand = self.observed_demand(
-            torch.sum(td["obs"]["observed_demand"].view(td.batch_size[0], -1), dim=-1, keepdim=True))
+        # Determine the shape based on dimensionality of current_demand
+        if td["done"].dim() == 2:
+            batch_size, _ = td["done"].shape
+            view_transform = lambda x: x.view(batch_size, -1)
+        elif td["done"].dim() == 3:
+            batch_size, n_step, _ = td["done"].shape
+            view_transform = lambda x: x.view(batch_size, n_step, -1)
+        else:
+            raise ValueError("Invalid shape of done in td")
 
-        # Vessel
-        residual_capacity = self.residual_capacity(td["obs"]["residual_capacity"].view(td.batch_size[0], -1))
-        origin_embed = self.origin_location(td["state"]["agg_pol_location"].view(td.batch_size[0], -1))
-        destination_embed = self.destination_location(td["state"]["agg_pod_location"].view(td.batch_size[0], -1))
+        # Demand embeddings
+        # todo: improve demand?
+        current_demand = self.current_demand(view_transform(td["obs"]["current_demand"]))
+        expected_demand = self.expected_demand(torch.sum(view_transform(td["obs"]["expected_demand"]), dim=-1, keepdim=True))
+        std_demand = self.std_demand(torch.sum(view_transform(td["obs"]["std_demand"]), dim=-1, keepdim=True))
+        observed_demand = self.observed_demand(torch.sum(view_transform(td["obs"]["observed_demand"]), dim=-1, keepdim=True))
 
-        # # Performance
-        overstowage = self.overstowage(td["obs"]["overstowage"].view(td.batch_size[0], -1))
-        excess_crane_moves = self.excess_crane_moves(td["obs"]["excess_crane_moves"].view(td.batch_size[0], -1))
-        total_loaded = self.total_loaded(td["obs"]["total_loaded"].view(td.batch_size[0], -1))
-
-        # # Feasibility
-        violation = self.violation(td["obs"]["violation"].view(td.batch_size[0], -1))
-        rhs = self.rhs(td["rhs"].view(td.batch_size[0], -1))
-        lhs_A = self.lhs_A(td["lhs_A"].view(td.batch_size[0], -1))
+        # Vessel embeddings
+        residual_capacity = self.residual_capacity(view_transform(td["obs"]["residual_capacity"]))
+        origin_embed = self.origin_location(view_transform(td["state"]["agg_pol_location"]))
+        destination_embed = self.destination_location(view_transform(td["state"]["agg_pod_location"]))
 
         # Concatenate all embeddings
         state_embed = torch.cat([
             current_demand, expected_demand, std_demand, observed_demand,
-            residual_capacity, origin_embed, destination_embed,
-            overstowage, excess_crane_moves, total_loaded,
-            violation, rhs, lhs_A
+            residual_capacity, origin_embed, destination_embed
         ], dim=-1)
         return state_embed
 
@@ -160,7 +164,7 @@ class DynamicSinusoidalPositionalEncoding(nn.Module):
         self.embed_dim = embed_dim
 
     def forward(self, x):
-        batch_size, seq_length, embed_dim = x.size()
+        _, seq_length, _ = x.size()
         position = torch.arange(seq_length, dtype=torch.float, device=x.device).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, self.embed_dim, 2, device=x.device).float() * -(math.log(10000.0) / seq_length))
         pe = torch.zeros(seq_length, self.embed_dim, device=x.device)
