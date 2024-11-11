b4426fffd2e2420779e3186b9a0adb3fb953d1c9
jaiv <jaiv@itu.dk>
2024-11-11 14:32:34 +0100

    update params

--- models/projection.py
+++ models/projection.py
@@ -73,12 +73,12 @@ class LinearViolationAdaption(th.nn.Module):
     def __init__(self, **kwargs):
         super(LinearViolationAdaption, self).__init__()
 
-    def forward(self, x, A, b, alpha=0.01, delta=0.05, tolerance=0.01):
+    def forward(self, x, A, b, alpha=0.025, delta=0.05, tolerance=0.05):
         batch_size, m = b.shape
         violation_old = th.zeros(batch_size, m, dtype=x.dtype, device=x.device)
         active_mask = th.ones(batch_size, dtype=th.bool, device=x.device)  # Start with all batches active
         x_ = x.clone()
-        # count = 0
+        count = 0
 
         if th.isnan(x_).any():
             return x_
@@ -107,7 +107,8 @@ class LinearViolationAdaption(th.nn.Module):
 
             # Update violation_old for the next iteration
             violation_old = violation_new.clone()
-            # count += 1
+            count += 1
+        print(f"Count: {count}")
         return x_
 
 
