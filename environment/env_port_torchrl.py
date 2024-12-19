7d773a862505ddc593b91182a184bc78fca64efc
jaiv <jaiv@itu.dk>
2024-12-19 17:16:10 +0100

    add rhs

--- environment/env_port_torchrl.py
+++ environment/env_port_torchrl.py
@@ -17,56 +17,46 @@ from environment.generator import MPP_Generator
 from environment.utils import *
 
 class PortMasterPlanningEnv(MasterPlanningEnv):
-    """Master Planning Problem environment."""
+    """Port-wise Master Planning Problem environment."""
     name = "port_mpp"
     batch_locked = False
 
     def __init__(self, device="cuda", batch_size=[], td_gen=None, **kwargs):
         super().__init__(device=device, batch_size=batch_size, **kwargs)
+        # Spec and td_gen
+        if td_gen == None:
+            self.td_gen = self.generator(batch_size=batch_size,)
+        self._make_spec(self.td_gen)
+
         # Revenue
         self.static_revenue = self.revenues_matrix.T # Shape [T, K]
 
-        # Constraints
-        # todo: implement A for portwise + clean-up
-        ones_cons = th.ones(self.n_constraints, device=self.generator.device, dtype=self.float_type)
-        self.constraint_signs = ones_cons.clone()
+        ## Constraints # todo: clean-up here
+        # Shapes
+        self.n_demand = (self.K * (self.P - 1))
+        self.n_locations = (self.B * self.D)
+        self.n_stability = 4
+        self.n_constraints = self.n_demand + self.n_locations + self.n_stability # (demand + capacity + stability)
+        # Ranges
+        self.demand_idx = th.arange(0, self.n_demand, device=device)
+        self.location_idx = th.arange(self.n_demand, self.n_demand+self.n_locations, device=device)
+        self.stability_idx = th.arange(self.n_demand+self.n_locations, self.n_constraints, device=device)
+        # Add vectors
+        ones = th.ones(self.n_constraints, device=self.generator.device, dtype=self.float_type)
+        self.constraint_signs = ones.clone()
         indices_to_multiply = th.tensor([-3, -1], device=self.generator.device)
         self.constraint_signs[indices_to_multiply] *= -1
-        self.swap_signs_stability = -ones_cons.clone() # swap signs for constraints
+        self.swap_signs_stability = -ones.clone()
         self.swap_signs_stability[0] *= -1 # only demand constraint is positive
-        # self.A = self._create_constraint_matrix(shape=(self.n_constraints, self.n_action, self.T, self.K))
+
+        # Get A
+        self.lhs_A = self._create_port_constraint_matrix(shape=(self.n_constraints, self.B, self.D, self.P-1, self.K))
+        self.rhs_A = self._create_port_util_constraint_matrix(shape=(self.n_constraints, self.B, self.D, self.T, self.K))
 
     def _make_spec(self, td:TensorDict = None) -> None:
         """Define the specs for observations, actions, rewards, and done flags."""
         batch_size = td.batch_size
         observation_spec = Unbounded(shape=(*batch_size,288), dtype=self.float_type)
-        #
-        # "state": {
-        #     # Vessel
-        #     "utilization": next_state_dict["utilization"].view(*batch_size, self.B * self.D * self.T * self.K),
-        #     "target_long_crane": next_state_dict["target_long_crane"].view(*batch_size, 1),
-        #     "long_crane_moves": next_state_dict["long_crane_moves"].view(*batch_size, self.B - 1),
-        #     # Demand
-        #     "observed_demand": next_state_dict["observed_demand"].view(*batch_size, self.T * self.K),
-        #     "expected_demand": next_state_dict["expected_demand"].view(*batch_size, self.T * self.K),
-        #     "std_demand": next_state_dict["std_demand"].view(*batch_size, self.T * self.K),
-        #     "realized_demand": next_state_dict["realized_demand"].view(*batch_size, self.T * self.K),
-        # },
-        # "observation": obs,
-        # "realized_demand": td["realized_demand"].view(*batch_size, self.T * self.K),
-        # "observed_demand": td["observed_demand"].view(*batch_size, self.T * self.K),
-        # "expected_demand": td["expected_demand"].view(*batch_size, self.T * self.K),
-        # "std_demand": td["std_demand"].view(*batch_size, self.T * self.K),
-        # "init_expected_demand": td["init_expected_demand"].view(*batch_size, self.T * self.K),
-        # "batch_updates": td["batch_updates"] + 1,
-        #
-        # # Action, reward, done and step
-        # "action": action.view(*batch_size, -1),
-        # "reward": reward,
-        # "done": done,
-        # "timestep": t,
-
-
         state_spec = Composite(
             utilization=Unbounded(shape=(*batch_size,self.B*self.D*self.T*self.K), dtype=self.float_type),
             target_long_crane=Unbounded(shape=(*batch_size,1), dtype=self.float_type),
@@ -120,6 +110,7 @@ class PortMasterPlanningEnv(MasterPlanningEnv):
         utilization, demand_state = self._extract_from_state(td["state"], batch_size)
 
         # Check done, update utilization and long cranes
+        # todo: add masking of action on irrelevant actions (when pod < pol)
         done = self._check_done(t)
         utilization = self._update_state_loading(action, utilization, load_idx, t[0])
         target_long_crane = compute_target_long_crane(demand_state["realized_demand"], moves_idx, self.capacity, self.B, self.CI_target)
@@ -145,22 +136,18 @@ class PortMasterPlanningEnv(MasterPlanningEnv):
         # todo: normalization of reward
         # normalize_revenue = load_revenues * load_demand
         # reward = (revenue_matrix.clone() / normalize_revenue).mean(dim=(-2,-1)) - (cost / normalize_revenue.sum()).clamp(max=10.0)
-        # print(reward, reward.shape)
 
         ## Transition to next step
         # Update next state
         t = t + 1
         next_state_dict = self._update_next_state(utilization, demand_state, t, batch_size)
+        if not done.any():
+            rhs = self.create_port_rhs(next_state_dict["utilization"], next_state_dict["rhs_demand"], batch_size)
 
-        # todo: implement lhs_A, rhs
-        # if not done.any():
-        #     # Update feasibility: lhs_A, rhs, clip_max based on next state
-        #     lhs_A = self.create_lhs_A(t,)
-        #     rhs = self.create_rhs(next_state_dict["utilization"], next_state_dict["current_demand"], batch_size)
-
-        # # Only for final port
-        if t[0] == self.P:
+        # Only for final port
+        if done:
             # Compute last port long crane excess
+            _, _, moves_idx = self._precompute_for_step(t[0])  # inherited!
             lc_moves_last_port = compute_long_crane(utilization, moves_idx, self.T)
             lc_excess_last_port = th.clamp(lc_moves_last_port - next_state_dict["target_long_crane"].view(-1,1), min=0)
 
@@ -186,6 +173,7 @@ class PortMasterPlanningEnv(MasterPlanningEnv):
                 "realized_demand": next_state_dict["realized_demand"].view(*batch_size, self.T * self.K),
             },
             "observation":obs,
+            # todo: fix redundancy demand?
             "realized_demand": td["realized_demand"].view(*batch_size, self.T * self.K),
             "observed_demand": td["observed_demand"].view(*batch_size, self.T*self.K),
             "expected_demand": td["expected_demand"].view(*batch_size, self.T*self.K),
@@ -215,12 +203,15 @@ class PortMasterPlanningEnv(MasterPlanningEnv):
         # Initialize
         device = td.device
         t = th.zeros((1,) if batch_size == th.Size([]) else (*batch_size,), dtype=th.int64, device=device)
-        _, _, moves_idx = self._precompute_for_step(t[0]) # inherited!
+        load_idx, _, moves_idx = self._precompute_for_step(t[0]) # inherited!
         action_mask = th.ones(self.action_spec.shape, dtype=th.bool, device=device)
         utilization = th.zeros((*batch_size, self.B, self.D, self.T, self.K), device=device, dtype=self.float_type)
         target_long_crane = compute_target_long_crane(td["realized_demand"].view(*batch_size, self.T, self.K),
                                                             moves_idx, self.capacity, self.B, self.CI_target)
         long_crane_moves = compute_long_crane(utilization, moves_idx, self.T)
+        rhs_demand = th.zeros((*batch_size, self.P-1, self.K), device=device, dtype=self.float_type)
+        rhs_demand[...,t:,:] = td["observed_demand"].view(*batch_size, self.T, self.K)[...,load_idx,:]
+        rhs = self.create_port_rhs(utilization, rhs_demand, batch_size)
         initial_state = TensorDict({
             "utilization": utilization.view(*batch_size, self.B*self.D*self.T*self.K),
             "target_long_crane": target_long_crane.view(*batch_size, 1),
@@ -237,9 +228,10 @@ class PortMasterPlanningEnv(MasterPlanningEnv):
             # State + obs
             "state": initial_state,
             "observation": obs,
-            # Action and mask
+            # Action, mask, rhs
             "action": th.zeros_like(action_mask, dtype=self.float_type),
             # "action_mask": action_mask.view(*batch_size, -1),
+            "rhs":rhs.view(*batch_size,-1),
             # Reward, done and step
             "done": th.zeros_like(t, dtype=th.bool),
             "timestep": t,
@@ -349,15 +341,64 @@ class PortMasterPlanningEnv(MasterPlanningEnv):
         # Update residual lc capacity: target - actual load and discharge moves
         residual_lc_capacity = (target_long_crane - long_crane_moves).clamp(min=0)
 
+        # rhs demand # todo: initialize rhs_demand constantly not efficient
+        rhs_demand = th.zeros((*batch_size, self.P - 1, self.K), device=self.device, dtype=self.float_type)
+        rhs_demand[..., t:, :] = demand_state["observed_demand"].view(*batch_size, self.T, self.K)[..., load_idx, :]
+
         # Get output
         return {
             "observed_demand": demand_state["observed_demand"],
             "expected_demand": demand_state["expected_demand"],
             "std_demand":demand_state["std_demand"],
             "realized_demand": demand_state["realized_demand"],
+            "rhs_demand":rhs_demand,
             "utilization": utilization,
             "location_utilization": (utilization * self.teus.view(1,1,1,1,-1)).sum(dim=(-2,-1)),
             "target_long_crane": target_long_crane,
             "long_crane_moves":long_crane_moves,
             "residual_lc_capacity": residual_lc_capacity,
         }
+
+    # Constraints
+    def _create_port_constraint_matrix(self, shape: Tuple):
+        """Create lhs matrix A in Ax <= b_t.
+            Shape [M, N]:
+            - M: [demand (P-1)*K, capacity B*D, LM-TW, TW-LM, VM-TW, TW-VM]
+            - N: action B*D*(P-1)*K
+        """
+        A = th.ones(shape, device=self.generator.device, dtype=self.float_type)
+        A[self.demand_idx] = th.eye(self.n_demand, device=self.generator.device, dtype=self.float_type).view(self.n_demand, 1, 1, self.P-1, self.K)
+        A[self.location_idx] = self.teus.view(1, 1, 1, -1) * th.eye(self.n_locations, device=self.generator.device, dtype=self.float_type).view(self.n_locations, self.B, self.D, 1, 1)
+        A[self.stability_idx] *= self.stability_params_lhs.view(self.n_stability, self.B, self.D, 1, self.K,)
+        return A.view(self.n_constraints, -1) * self.constraint_signs.unsqueeze(-1)
+
+    def _create_port_util_constraint_matrix(self, shape: Tuple):
+        """Create constraint matrix A' as part of rhs vector: b_t = b - A'u_t.
+        Shape [M, N]:
+        - M: [demand (P-1)*K, capacity B*D, LM-TW, TW-LM, VM-TW, TW-VM]
+        - N: utilization B*D*T*K
+
+        Note this is only relevant for capacity and stability constraints, as it depends on the current utilization.
+        """
+        A = th.ones(shape, device=self.generator.device, dtype=self.float_type)
+        A[self.demand_idx] = 0 # set to 0, as irrelevant for demand
+        A[self.location_idx] = self.teus.view(1, 1, 1, -1) * th.eye(self.n_locations, device=self.generator.device, dtype=self.float_type).view(self.n_locations, self.B, self.D, 1, 1)
+        A[self.stability_idx] *= self.stability_params_lhs.view(self.n_stability, self.B, self.D, 1, self.K,)
+        return A.view(self.n_constraints, -1) * self.constraint_signs.unsqueeze(-1)
+
+    def create_port_rhs(self, utilization, observed_demand, batch_size):
+        """Create rhs vector b_t in Ax <= b_t.
+            Shape [M]:
+            - M: [demand (P-1)*K, capacity B*D, LM-TW, TW-LM, VM-TW, TW-VM]
+
+        Note: b_t = b - A'u_t, where b is some vector, A'u_t is matmul of rhs constraint matrix and utilization u_t.
+        """
+        # Compute -A'u_t
+        A = self.swap_signs_stability.view(-1, 1) * self.rhs_A.clone() # Obtain -A'
+        b_t = utilization.view(*batch_size, -1) @ A.view(self.n_constraints, -1).T # Matmul -A'u_t
+        # Compute b_t = b - A'u_t
+        b_t[..., self.demand_idx] = observed_demand.view(*batch_size, -1)
+        capacity = self.capacity.view(*batch_size, -1)
+        b_t[..., self.location_idx] = th.clamp(b_t[..., self.location_idx] + capacity,
+                                               min=th.zeros_like(capacity, device=self.device), max=capacity)
+        return b_t
