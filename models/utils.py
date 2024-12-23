3f40e427468672c2faa29af0cde13cadf24c060a
jaiv <jaiv@itu.dk>
2024-12-23 13:53:39 +0100

    add

---
+++ models/utils.py
@@ -0,0 +1,32 @@
+from typing import Optional
+from dotmap import DotMap
+import torch
+from environment.env_torchrl import MasterPlanningEnv
+from environment.utils import compute_violation
+
+
+## Helper functions
+def make_env(env_kwargs:DotMap, batch_size:Optional[list] = [], device: torch.device = torch.device("cuda")):
+    """Setup and transform the Pendulum environment."""
+    return MasterPlanningEnv(batch_size=batch_size, **env_kwargs).to(device)
+
+def adapt_env_kwargs(config):
+    """Adapt environment kwargs based on configuration"""
+    config.env.bays = 10 if config.env.TEU == 1000 else 20
+    config.env.weight_classes = 3 if config.env.cargo_classes % 3 == 0 else 2 # 2 weights for 2 classes, 3 weights for 3,6 classes
+    config.env.capacity = [50] if config.env.TEU == 1000 else [500]
+    return config
+
+# todo: redudant - also in loss_module of PPO_feas
+def compute_loss_feasibility(td, action, feasibility_coef, aggregate_feasibility="sum"):
+    """Compute feasibility loss based on the action, lhs_A, and rhs tensors."""
+    lhs_A = td.get("lhs_A")
+    rhs = td.get("rhs")
+    violation = compute_violation(action, lhs_A, rhs)
+
+    # Get aggregation dimensions
+    if aggregate_feasibility == "sum":
+        sum_dims = [-x for x in range(1, violation.dim())]
+        return feasibility_coef * violation.sum(dim=sum_dims).mean(), violation
+    elif aggregate_feasibility == "mean":
+        return feasibility_coef * violation.mean(), violation
