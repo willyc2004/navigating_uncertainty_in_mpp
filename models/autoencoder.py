7d411434eb34b0b0a234b648e89528dd887a8821
jaiv <jaiv@itu.dk>
2025-01-06 12:03:05 +0100

    create autoencoder

---
+++ models/autoencoder.py
@@ -0,0 +1,17 @@
+from torch import nn
+from models.decoder import AttentionDecoderWithCache
+
+class Autoencoder(nn.Module):
+    def __init__(self, encoder, decoder, env):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.env = env
+
+    def forward(self, obs):
+        hidden, init_embed = self.encoder(obs)
+        print("obs", type(obs))
+        if isinstance(self.decoder, AttentionDecoderWithCache):
+            _, _, hidden = self.decoder.pre_decoder_hook(obs, self.env, hidden)
+        dec_out = self.decoder(obs, hidden)
+        return dec_out
