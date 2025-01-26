81256b5c6d27f6ade51e217bef960d5da08a9f99
jaiv <jaiv@itu.dk>
2025-01-26 11:02:26 +0100

    update

--- test_results/scenario_tree/analyze_results.py
+++ test_results/scenario_tree/analyze_results.py
@@ -2,11 +2,10 @@ import json
 import numpy as np
 
 # Load JSON file
-s = 28
-perfect_information = True
-folder = 'generalization'
-with open(f'{folder}/results_scenario_tree_s{s}_pi{perfect_information}.json', 'r') as file:
-    data = json.load(file)
+s = 20
+episodes = 30
+perfect_information = False
+gen = False
 
 obj = []
 time = []
@@ -14,13 +13,34 @@ total_load = []
 total_demand = []
 total_revenue = []
 total_cost = []
-for x in data:
-    obj.append(x["obj"])
-    time.append(x["time"])
-    total_load.append(np.sum(x["mean_load_per_port"], axis=-1))
-    total_demand.append(np.sum(x["mean_demand"], axis=-1))
-    total_revenue.append(np.sum(x["mean_revenue"], axis=-1))
-    total_cost.append(np.sum(x["mean_cost"], axis=-1))
+for e in range(episodes):
+    with open(f'results_scenario_tree_e{e}_s{s}_pi{perfect_information}_gen{gen}.json', 'r') as file:
+        x = json.load(file)
+        obj.append(x["obj"])
+        time.append(x["time"])
+        total_load.append(np.sum(x["mean_load_per_port"], axis=-1))
+        total_demand.append(np.sum(x["mean_demand"], axis=-1))
+        total_revenue.append(np.sum(x["mean_revenue"], axis=-1))
+        total_cost.append(np.sum(x["mean_cost"], axis=-1))
+
+
+# folder = 'generalization'
+# with open(f'{folder}/results_scenario_tree_s{s}_pi{perfect_information}.json', 'r') as file:
+#     data = json.load(file)
+
+# obj = []
+# time = []
+# total_load = []
+# total_demand = []
+# total_revenue = []
+# total_cost = []
+# for x in data:
+#     obj.append(x["obj"])
+#     time.append(x["time"])
+#     total_load.append(np.sum(x["mean_load_per_port"], axis=-1))
+#     total_demand.append(np.sum(x["mean_demand"], axis=-1))
+#     total_revenue.append(np.sum(x["mean_revenue"], axis=-1))
+#     total_cost.append(np.sum(x["mean_cost"], axis=-1))
 
 # Helper function to print statistics
 def print_stats(data, name=""):
