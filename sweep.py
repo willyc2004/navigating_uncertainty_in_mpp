1a9f71351f62e7bd8c972b5db35c65b253ffbfd6
jaiv <jaiv@itu.dk>
2025-07-21 21:46:21 +0200

    run ppo-pd sweep

--- sweep.py
+++ sweep.py
@@ -49,20 +49,20 @@ if __name__ == "__main__":
             # # config['algorithm']['entropy_lambda'] = sweep_config.entropy_lambda
             #
             # # # AM-PPO hyperparameters
-            config['algorithm']['feasibility_lambda'] = sweep_config.feasibility_lambda
+            # config['algorithm']['feasibility_lambda'] = sweep_config.feasibility_lambda
             # config['training']['lr'] = sweep_config.lr
-            # config['training']['pd_lr'] = sweep_config.pd_lr
+            config['training']['pd_lr'] = sweep_config.pd_lr
             # config['training']['projection_kwargs']['alpha'] = sweep_config.alpha
             # config['training']['projection_kwargs']['delta'] = sweep_config.delta
             # config['training']['projection_kwargs']['max_iter'] = sweep_config.max_iter
             # config['training']['projection_kwargs']['scale'] = sweep_config.scale
 
-            # Algorithm hyperparameters
-            for i in range(n_constraints):
-                config['algorithm'][f'lagrangian_multiplier_{i}'] = sweep_config[f'lagrangian_multiplier_{i}']
-                # Error handling for missing lagrangian multipliers
-                if f'lagrangian_multiplier_{i}' not in sweep_config:
-                    raise ValueError(f"Missing lagrangian_multiplier_{i} in sweep configuration")
+            # # Algorithm hyperparameters
+            # for i in range(n_constraints):
+            #     config['algorithm'][f'lagrangian_multiplier_{i}'] = sweep_config[f'lagrangian_multiplier_{i}']
+            #     # Error handling for missing lagrangian multipliers
+            #     if f'lagrangian_multiplier_{i}' not in sweep_config:
+            #         raise ValueError(f"Missing lagrangian_multiplier_{i} in sweep configuration")
 
             # Call your main() function
             model = main(config)
